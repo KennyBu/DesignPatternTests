d01d6a54750af2f62cc098fa9ce4b292a1614e78
Ken Burkhardt <ken.burkhardt@gmail.com>
2011-08-10 22:35:27 -0400

    latest updated

--- DecoratorTest/ISendText.cs
+++ DecoratorTest/ISendText.cs
@@ -1,14 +1,7 @@
-using System;
-
 namespace DecoratorTest
 {
     public interface ISendText
     {
         void SendText(string textToSend);
-    }
-
-    public interface ISendEmailText : ISendText
-    {
-        
     }
 }
